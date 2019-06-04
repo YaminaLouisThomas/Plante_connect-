ad5a9f92afae1235eea958faf9a40f564855f42f
loux22 <louis.ardilly@yahoo.fr>
2019-06-04 23:25:51 +0200

    add connected database with C

---
+++ Rendu5/programme-C-plante/plante.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void alisier_blanc(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void guillmauve_officinale(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void centaure_noire(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void muguet(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void digitale_pourpre(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void asperule_odorante(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void hysope(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void iris_faux_acore(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void menthe_a_feuilles_rondes(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void petite_pervenche(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void violette_des_chiens(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void origan_commun(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void lysimaque_commune(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void gesse_des_bois(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void knautie_des_champs(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void jasione_des_montagnes(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void aubepine_a_un_style(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void calamagrostide_commun(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void campanule_agglomeree(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void laiche_aigue(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void fetuque_des_moutons(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void heliantheme_des_apennins(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void piloselle_officinale(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void millepertuis_perfore(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void luzule_des_bois(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void osmonde_royale(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void raiponce_en_epi(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void polypode_commun(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
+
+void anemone_pulsatille(int humidite, int pourcentage,int temperature,int temperature_requise,int luminosite, int luminoste_requise);
